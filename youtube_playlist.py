ee4f9e12d582ac03b8dc0304e97c3018f01dd835
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-03-15 15:56:58 +0530

    Refactor YouTube playlist handling and file operations

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -1,145 +1,79 @@
 import datetime
 import os
-from typing import ItemsView
 import pickle
 from googleapiclient.discovery import build
 
 youtube_api_key = os.environ.get("YOUTUBE_API_KEY")
-playlist_ids    = {"PLK5tc6FSo174pECpHWftUYDcw5KFk4HLs": "Gym", "PLK5tc6FSo175xc8zNBMrUZJIY9Q_K9I4w": "Driving", "PLK5tc6FSo177DVG_k_Tx57Ztvh0B-5Drd": "Songs"}
-# channel_id      = "your_channel_id"
+if not youtube_api_key:
+    raise ValueError("YOUTUBE_API_KEY environment variable not set")
 
-youtube = build('youtube', 'v3', developerKey=youtube_api_key)
+playlist_ids = {
+    "PLK5tc6FSo174pECpHWftUYDcw5KFk4HLs": "Gym",
+    "PLK5tc6FSo175xc8zNBMrUZJIY9Q_K9I4w": "Driving",
+    "PLK5tc6FSo177DVG_k_Tx57Ztvh0B-5Drd": "Songs"
+}
 
-#------------------------------------------------------------------------#
-# pl_request = youtube.playlists().list(
-#     part= 'contentDetails, snippet',
-#     channelId = channel_id,
-#     maxResults= 10
-#         )
+youtube      = build('youtube', 'v3', developerKey=youtube_api_key)
+current_time = datetime.datetime.now()
 
-# pl_response = pl_request.execute()
+for playlist_id, playlist_name in playlist_ids.items():
+    video_ID_list    = []
+    video_title_list = []
+    nextPageToken    = None
 
-# for item in pl_response['items']:
-#     print(item)
+    while True:
+        pl_response = youtube.playlistItems().list(
+            part       = 'contentDetails',
+            playlistId = playlist_id,
+            maxResults = 50,
+            pageToken  = nextPageToken
+        ).execute()
 
-#     print( item['snippet']['title'] )
-#------------------------------------------------------------------------#
-current_time     = datetime.datetime.now()
+        vid_ids = [item["contentDetails"]["videoId"] for item in pl_response['items']]
+        video_ID_list.extend(vid_ids)
 
-nextPageToken    = None
+        vid_response = youtube.videos().list(
+            part       = "snippet",
+            id         = ','.join(vid_ids),
+            maxResults = 50
+        ).execute()
 
-for playlist_id in playlist_ids:
-    video_ID_list    = [ ]
-    video_title_list = [ ]   #List that holds all the titles of the videos of a playlist
-    while True:           #Loop through multiple pages of 50-values
-    
-        pl_request = youtube.playlistItems().list(
-                part       ='contentDetails',
-                playlistId = playlist_id,
-                maxResults = 50,
-                pageToken  = nextPageToken
-            )
-    
-        pl_response = pl_request.execute()
-    
-        vid_ids = [ ]      #Save Video IDs from that page into a list
-    
-        for item in pl_response['items']:
-            vid_ids.append ( item["contentDetails"]["videoId"] )
-            video_ID_list.append ( item["contentDetails"]["videoId"] )
-    
-        # print(','.join(vid_ids))   #separate all values in a list with commas
-        #------------------------------------------------------------------------#
-        vid_request = youtube.videos().list(
-                part       = "contentDetails, snippet",
-                id         = ','.join(vid_ids),
-                maxResults = 50
-            )
-    
-        vid_response = vid_request.execute()
-    
-        for item in vid_response['items']:
-            # print(item['snippet']['title'])
-            video_title_list.append(item['snippet']['title'])
-    
+        video_title_list.extend([item['snippet']['title'] for item in vid_response['items']])
+
         nextPageToken = pl_response.get("nextPageToken")
         if not nextPageToken:
             break
-    #---------------------------------------------------------------------------#
-    yt_video_dict_list = {str(videoId):str(video_tittle) for videoId,video_tittle in zip(video_ID_list,video_title_list) }
-    #---------------------------------------------------------------------------#
-    # ```Test if files exist. If not create them.```
-    
-    if os.path.isfile('Video_Titles.txt'):
-        print ("Video_Titles.txt file exists.")
-    else:
-        print ("Video_Titles.txt file does not exist!...creating the file")
-        with open('Video_Titles.txt', 'w') as f:
-            pass
-    
-    if os.path.isfile('Video_Playlist_Data.p'):
-        print ("Video_Playlist_Data.p file exists.")
-    else:
-        print ("Video_Playlist_Data.p file does not exist!...creating the file")
-        with open('Video_Playlist_Data.p', 'w') as f:
-            pass
-    
-    if os.path.isfile('Video_Titles_Added.txt'):
-        print ("Video_Titles_Added.txt file exists.")
-    else:
-        print ("Video_Titles_Added file does not exist!...creating the file")
-        with open('Video_Titles_Added.txt', 'w') as f:
-            pass
-    
-    if os.path.isfile('Video_Titles_Removed.txt'):
-        print ("Video_Titles_Removed.txt file exists.")
-    else:
-        print ("Video_Titles_Removed.txt file does not exist!...creating the file")
-        with open('Video_Titles_Removed.txt', 'w') as f:
-            pass
-    #---------------------------------------------------------------------------#
-    
 
-    data_file = f'{playlist_ids[playlist_id]}_Video_Playlist_Data.p'
+    current = {vid: title for vid, title in zip(video_ID_list, video_title_list)}
 
+    data_file = f"{playlist_name}_Video_Playlist_Data.p"
     if os.path.exists(data_file):
-        with open(data_file, 'rb') as file:
-            saved_video_dict_list = pickle.load(file)
+        with open(data_file, 'rb') as f:
+            previous = pickle.load(f)
     else:
-        print(f"{data_file} not found...creating")
-        saved_video_dict_list = {}
-    
-    songs_ids_added   = [ song_id for song_id in yt_video_dict_list    if song_id not in saved_video_dict_list ]
-    songs_ids_removed = [ song_id for song_id in saved_video_dict_list if song_id not in yt_video_dict_list    ]
-    songs_added       = [ yt_video_dict_list.get   (song, "Error") for song in songs_ids_added ]
-    songs_removed     = [ saved_video_dict_list.get(song, "Error") for song in songs_ids_removed ]
-    
-    if songs_added:
-        with open(f"{playlist_ids[playlist_id]}_Video_Titles_Added.txt", "a+", encoding="utf-8") as text_file:
-            text_file.write( f"Songs Added on: {current_time} " + "\n\n")
-            for index, song in enumerate(songs_added, 1):
-                text_file.write(str(index) + ": " + str(song) + "\n")
-            text_file.write( "#-----------------------------------------------#\n\n")
-    
-    if songs_removed:
-        with open(f"{playlist_ids[playlist_id]}_Video_Titles_Removed.txt", "a+", encoding="utf-8") as text_file:
-            text_file.write( f"Songs Removed on: {current_time} " + "\n\n")
-            for index, song in enumerate(songs_removed, 1):
-                text_file.write(str(index) + ": " + str(song) + "\n")
-            text_file.write( "#-----------------------------------------------#\n\n")
-    
-    #---------------------------------------------------------------------------#
-    
-    with open(f'{playlist_ids[playlist_id]}_Video_Playlist_Data.p', 'wb') as file:
-        pickle.dump(yt_video_dict_list, file, protocol=pickle.HIGHEST_PROTOCOL)
-    
-    with open(f"{playlist_ids[playlist_id]}_Video_Titles.txt", "w", encoding="utf-8") as text_file:
-        text_file.write( f"Playlist last checked on: {current_time} " + "\n\n")
-        for index, song in enumerate(list(yt_video_dict_list.values()), 1):
-             text_file.write(str(index) + ": " + str(song) + "\n")   
-    
-    # if __name__ == "__main__":
-    #     read_files()
-    #     get_youtube_data()
-    #     update_files()
-    #     print("Done")
+        previous = {}
+
+    added   = {sid: current[sid]  for sid in current  if sid not in previous}
+    removed = {sid: previous[sid] for sid in previous if sid not in current}
+
+    if added:
+        with open(f"{playlist_name}_Video_Titles_Added.txt", "a", encoding="utf-8") as f:
+            f.write(f"Added on: {current_time}\n\n")
+            for i, title in enumerate(added.values(), 1):
+                f.write(f"{i}: {title}\n")
+            f.write("#-----------------------------------------------#\n\n")
+
+    if removed:
+        with open(f"{playlist_name}_Video_Titles_Removed.txt", "a", encoding="utf-8") as f:
+            f.write(f"Removed on: {current_time}\n\n")
+            for i, title in enumerate(removed.values(), 1):
+                f.write(f"{i}: {title}\n")
+            f.write("#-----------------------------------------------#\n\n")
+
+    with open(data_file, 'wb') as f:
+        pickle.dump(current, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    with open(f"{playlist_name}_Video_Titles.txt", "w", encoding="utf-8") as f:
+        f.write(f"Playlist last checked on: {current_time}\n\n")
+        for i, title in enumerate(current.values(), 1):
+            f.write(f"{i}: {title}\n")
